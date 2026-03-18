9904b1d2ee4e7d1d62ef5c00c98f729ae3b848c0
AMkuro <4324018@ed.tus.ac.jp>
2026-03-18 20:55:46 +0900

    add attachment file

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,17 @@
+def main() -> None:
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+    access_file_name: str = "ancient_fragment.txt"
+    print(f"Accessing Storage Vault: {access_file_name}")
+    try:
+        with open(f"../attachments/{access_file_name}") as f:
+            print("Connection established...\n")
+            sentence = f.read()
+            print("RECOVERED DATA:")
+            print(f"{sentence}")
+        print("Data recovery complete. Storage unit disconnected.")
+    except FileNotFoundError:
+        print("ERROR: Storage vault not found. Run data generator first.")
+
+
+if __name__ == "__main__":
+    main()
