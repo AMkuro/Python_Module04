d3430fdc366ad6bc4dde4f8feebc5fd34ff2fb8a
AMkuro <4324018@ed.tus.ac.jp>
2026-03-20 13:01:27 +0900

    change with statements to open() close()

--- ex0/ft_ancient_text.py
+++ ex0/ft_ancient_text.py
@@ -3,13 +3,12 @@ def main() -> None:
     access_file_name: str = "ancient_fragment.txt"
     print(f"Accessing Storage Vault: {access_file_name}")
     try:
-        with open(
-            f"../attachments/{access_file_name}", encoding="us-ascii"
-        ) as f:
-            print("Connection established...\n")
-            sentence = f.read()
-            print("RECOVERED DATA:")
-            print(sentence)
+        f = open(f"../attachments/{access_file_name}", encoding="us-ascii")
+        print("Connection established...\n")
+        sentence = f.read()
+        print("RECOVERED DATA:")
+        print(sentence)
+        f.close()
         print("\nData recovery complete. Storage unit disconnected.")
     except FileNotFoundError:
         print("ERROR: Storage vault not found. Run data generator first.")
--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -3,22 +3,21 @@ def main() -> None:
     new_file_name: str = "new_discovery.txt"
     print(f"Initializing new storage unit: {new_file_name}")
     try:
-        with open(
-            f"../attachments/{new_file_name}", encoding="us-ascii", mode="w"
-        ) as f:
-            print("Storage unit created successfully...\n")
-            print("Inscribing preservation data...")
-            entries: list[str] = [
-                "New quantum algorithm discovered",
-                "Efficiency increased by 347%",
-                "Archived by Data Archivist trainee",
-            ]
-            entry_num = 1
-            for text in entries:
-                entry = f"[ENTRY {entry_num:03d}] {text}"
-                f.write(f"{entry}\n")
-                print(entry)
-                entry_num += 1
+        f = open(f"../attachments/{new_file_name}", mode="w", encoding="us-ascii")
+        print("Storage unit created successfully...\n")
+        print("Inscribing preservation data...")
+        entries: list[str] = [
+            "New quantum algorithm discovered",
+            "Efficiency increased by 347%",
+            "Archived by Data Archivist trainee",
+        ]
+        entry_num = 1
+        for text in entries:
+            entry = f"[ENTRY {entry_num:03d}] {text}"
+            f.write(f"{entry}\n")
+            print(entry)
+            entry_num += 1
+        f.close()
         print("\nData inscription complete. Storage unit sealed.")
         print(f"Archive '{new_file_name}' ready for long-term preservation.")
     except FileNotFoundError:
